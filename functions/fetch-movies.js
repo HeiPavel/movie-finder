6d4f3906cbd1c1688145b1a361012a490d2664e1
HeiPavel <112711905HeiPavel@users.noreply.github.com>
2023-08-25 02:48:21 +0300

    Back to fetching only specific movies by complex filter terms

--- functions/fetch-movies.js
+++ functions/fetch-movies.js
@@ -3,7 +3,7 @@ import fetch from "node-fetch";
 const fetchByParams = async (baseUrl, queryObj, apiKey) => {
     let params = new URLSearchParams();
     for (const param in queryObj) {
-        if (queryObj[param] && param !== 'query' && param !== 'separator') params.set(param, queryObj[param]);
+        if (queryObj[param] && param !== 'query') params.set(param, queryObj[param]);
     }
     const url = `${baseUrl}discover/movie?api_key=${apiKey}&${params.toString()}`;
     try {
@@ -29,28 +29,11 @@ const fetchByTitle = async (baseUrl, queryObj, apiKey) => {
     }
 }
 
-const fetchByActors = async (baseUrl, queryObj, apiKey) => {
-    console.log('hello');
-    const {page, with_people} = queryObj;
-    const query = with_people.split(/[,|\|]/).join('|');
-    const url = `${baseUrl}discover/movie?api_key=${apiKey}&with_people=${query}&page=${page}`;
-    try {
-        const response = await fetch(url);
-        const jsonResponse = await response.json();
-        return jsonResponse;
-    } catch(error) {
-        console.log(error);
-    }
-}
-
 const handler = async (event) => {
-    const {with_people, with_genres, primary_release_year, separator} = event.queryStringParameters;
     const apiKey = process.env.API_KEY;
     const baseUrl = "https://api.themoviedb.org/3/";
-    const promisesToFetch = [fetchByTitle(baseUrl, event.queryStringParameters, apiKey), fetchByParams(baseUrl, event.queryStringParameters, apiKey)];
-    if ((with_people && (with_genres || primary_release_year)) || (separator && with_people && with_people.split(/[,|\|]/).length > 1)) promisesToFetch.push(fetchByActors(baseUrl, event.queryStringParameters, apiKey));
     try {
-        const response = await Promise.all(promisesToFetch);
+        const response = await Promise.all([fetchByTitle(baseUrl, event.queryStringParameters, apiKey), fetchByParams(baseUrl, event.queryStringParameters, apiKey)]);
         const data = [];
         response.forEach(collection => data.push(...collection.results));
         return {
--- src/util/moviesRequest.js
+++ src/util/moviesRequest.js
@@ -4,7 +4,6 @@ export const fetchMovies = async (obj) => {
         const stringParam =  (typeof obj[param] === 'object') ? obj[param].array.join(`${obj[param].separator ? ',' : '|'}`) : obj[param];
         if (stringParam) params.set(param, stringParam);
     }
-    if (obj['with_people'].array.length && obj['with_people'].separator) params.set('separator', 'true');
     try {
         const response = await fetch(`.netlify/functions/fetch-movies?${params.toString()}`);
         const json = await response.json();
