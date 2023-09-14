ea5cdc3d13b90fb75c7751a23cdbdc361c76661b
HeiPavel <112711905HeiPavel@users.noreply.github.com>
2023-09-14 19:51:01 +0300

    Trying to redirect by react router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { MainPage } from "../mainPage/MainPage";
 import { MoviePage } from "../moviePage/MoviePage";
-import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route, Navigate } from "react-router-dom";
 
 const router = createBrowserRouter(createRoutesFromElements(
   <>
     <Route path="/" element={<MainPage/>} />
+    <Route path="/index.html" element={<Navigate to="/" />} />
     <Route path="/movie/:id" element={<MoviePage />} />
   </>
 ));
